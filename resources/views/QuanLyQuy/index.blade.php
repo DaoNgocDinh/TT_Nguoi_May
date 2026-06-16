1c8cd90dfc2202eaaff2ae28e06252ecd9dfb3c0
Tên của bạn <dinhdaongoc18@gmail.com>
2026-06-16 21:40:16 +0700

    DND fix 1

--- resources/views/QuanLyQuy/index.blade.php
+++ resources/views/QuanLyQuy/index.blade.php
@@ -252,12 +252,12 @@
 
                                 <div class="flex justify-center gap-4 mt-4">
                                     <button
-                                        class="acceptBtn w-[99px] h-[38px] rounded-full border-2 border-[#81FF73] bg-[#81FF73] text-white text-[18px] transition-all duration-300">
+                                        class="acceptBtn w-[99px] h-[38px] rounded-full border-2 border-[#81FF73] bg-[#81FF73] text-white !text-[18px] transition-all duration-300">
                                         Đồng ý
                                     </button>
 
                                     <button
-                                        class="rejectBtn w-[99px] h-[38px] rounded-full border-2 border-[#E7000B] bg-white text-[#E7000B] text-[18px] transition-all duration-300">
+                                        class="rejectBtn w-[99px] h-[38px] rounded-full border-2 border-[#E7000B] bg-white text-[#E7000B] !text-[18px] transition-all duration-300">
                                         Từ chối
                                     </button>
                                 </div>
@@ -295,9 +295,9 @@
                                 <div class="flex justify-center gap-4 mt-4">
 
                                     <button
-                                        class="deleteFundBtn flex w-[99px] h-[38px] rounded-full border-2 border-[#FC0005] bg-[#FFFFFF] text-[#FC0005] text-[18px] items-center justify-center transition-all duration-200 hover:bg-[#FC0005] hover:text-white hover:shadow-md cursor-pointer">
+                                        class="deleteFundBtn flex w-[99px] h-[38px] rounded-full border-2 border-[#FC0005] bg-[#FFFFFF] text-[#FC0005] !text-[18px] items-center justify-center transition-all duration-200 hover:bg-[#FC0005] hover:text-white hover:shadow-md cursor-pointer">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-[18px]">
+                                            stroke-width="1.5" stroke="currentColor" class="!size-[18px]">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
@@ -339,21 +339,21 @@
 
             <div class="flex justify-center gap-8 pb-5">
 
-                <button id="backBtn" class="w-[126px] h-[38px] rounded-full
-                border-2 border-[#D42A87]
-                bg-[#D42A87]
-                text-white text-[18px]
-                transition-all duration-300">
-                    Quay lại
-                </button>
-
                 <button id="confirmBtn" class="w-[126px] h-[38px] rounded-full
                 border-2 border-[#D42A87]
                 bg-white
                 text-[#D42A87]
-                text-[18px]
+                !text-[20px]
                 transition-all duration-300">
                     Xác nhận
+                </button>
+
+                <button id="backBtn" class="w-[126px] h-[38px] rounded-full
+                border-2 border-[#D42A87]
+                bg-[#D42A87]
+                text-white !text-[20px]
+                transition-all duration-300">
+                    Quay lại
                 </button>
 
             </div>
@@ -384,16 +384,15 @@
 
             <div class="flex justify-center gap-8 pb-5">
 
-                <button id="deleteBackBtn" class="w-[126px] h-[38px] rounded-full border-2 border-[#D42A87]
-    bg-[#D42A87] text-white text-[18px]
-    transition-all duration-300">
-                    Quay lại
-                </button>
-
                 <button id="deleteConfirmBtn" class="w-[126px] h-[38px] rounded-full border-2 border-[#D42A87]
-    bg-white text-[#D42A87] text-[18px]
+    bg-white text-[#D42A87] !text-[18px]
     transition-all duration-300">
                     Xác nhận
+                </button>
+                <button id="deleteBackBtn" class="w-[126px] h-[38px] rounded-full border-2 border-[#D42A87]
+    bg-[#D42A87] text-white !text-[18px]
+    transition-all duration-300">
+                    Quay lại
                 </button>
 
             </div>

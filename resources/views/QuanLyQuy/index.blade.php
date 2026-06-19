be94a5e80949b2510d1451917c38bfda4d5c4727
Tên của bạn <dinhdaongoc18@gmail.com>
2026-06-19 09:27:16 +0700

    DND TAT DHD

--- resources/views/QuanLyQuy/index.blade.php
+++ resources/views/QuanLyQuy/index.blade.php
@@ -339,6 +339,14 @@
 
             <div class="flex justify-center gap-8 pb-5">
 
+                <button id="backBtn" class="w-[126px] h-[38px] rounded-full
+                border-2 border-[#D42A87]
+                bg-[#D42A87]
+                text-white !text-[20px]
+                transition-all duration-300">
+                    Quay lại
+                </button>
+
                 <button id="confirmBtn" class="w-[126px] h-[38px] rounded-full
                 border-2 border-[#D42A87]
                 bg-white
@@ -346,14 +354,6 @@
                 !text-[20px]
                 transition-all duration-300">
                     Xác nhận
-                </button>
-
-                <button id="backBtn" class="w-[126px] h-[38px] rounded-full
-                border-2 border-[#D42A87]
-                bg-[#D42A87]
-                text-white !text-[20px]
-                transition-all duration-300">
-                    Quay lại
                 </button>
 
             </div>
